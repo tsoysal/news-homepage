62c8ba89f1f1b2098aaca513098c7b531c4a073c
elif kurt <berksysl.g@gmail.com>
2024-01-05 19:15:58 +0300

    Create responsive functional navigation menu

--- src/mobileMenu.js
+++ src/mobileMenu.js
@@ -1,3 +1,11 @@
 const menuBtn = document.querySelector('.mobile-menu-btn')
 const menuCloseBtn = document.querySelector('.mobile-menu-close-btn')
 const menu = document.querySelector('.mobile-menu');
+
+menuBtn.addEventListener('click', ()=> {
+    menu.style.display = "block";
+})
+
+menuCloseBtn.addEventListener('click', ()=> {
+    menu.style.display = "none";
+})
