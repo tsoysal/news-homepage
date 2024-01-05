bf042fa75db28c6eb7e9416708ab8dd5f4010eaa
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-01-05 16:17:00 +0300

    add mobile menu

---
+++ src/mobileMenu.js
@@ -0,0 +1,3 @@
+const menuBtn = document.querySelector('.mobile-menu-btn')
+const menuCloseBtn = document.querySelector('.mobile-menu-close-btn')
+const menu = document.querySelector('.mobile-menu');
